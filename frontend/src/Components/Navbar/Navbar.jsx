6a2f2c65186a5dd65d14bb5431c439c3afb016ef
Matheus <90159525+oftheus@users.noreply.github.com>
2024-12-09 09:39:45 -0300

    docs: add comments to navbar component

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,14 +4,21 @@ import './Navbar.css';
 import brasao from '../../assets/brasao.png';
 
 const Navbar = () => {
+
+  //Estado para controlar se a navbar deve ser sticky com base no scroll
   const [sticky, setSticky] = useState(false);
+
+  // Hook para navegação entre páginas
   const navigate = useNavigate(); 
 
+  // useEffect que adiciona um listener de evento para monitorar o scroll da janela
   useEffect(() => {
+    //adiciona um evento de scroll na janela
     window.addEventListener('scroll', () => {
+      // Define o estado sticky com base na posição do scroll vertical
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     });
-  }, []);
+  }, []); //Array de dependências vazio para executar apenas no carregamento inicial
 
   // Função para redirecionar ao Dashboard ao clicar em "Entrar" (Provisório)
   const irParaDashboard = () => {
@@ -19,8 +26,12 @@ const Navbar = () => {
   };
 
   return (
+    // Contêiner principal da navbar
+    // Aplica a classe 'navbar-escura' dinamicamente se o estado "sticky" for verdadeiro
     <nav className={`container-fluid ${sticky ? 'navbar-escura' : ''}`}>
+      {/* Logo da Navbar (brasão da UFF) */}
       <img src={brasao} alt="Brasão UFF" className='brasao' />
+      {/* Lista de opções da navbar */}
       <ul>
         <li onClick={irParaDashboard} style={{ cursor: 'pointer' }}>Entrar</li>
         <li><button className='botao2'>Anunciar grátis</button></li>
